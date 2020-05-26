0a458ad18a9aea1211e8951a240957b03b0ba1c5
Kim Vo <kimlongt.vo@gmail.com>
2020-05-26 13:32:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect,  } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+    useEffect(() => {
+        //  Init Materialize JS
+        M.AutoInit();
+    });
+    return (
+        <>
+            <SearchBar />
+            <div className="container">
+              <Logs />
+            </div>
+        </>
+    );
+};
+
+export default App;
