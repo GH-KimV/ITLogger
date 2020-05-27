8d005315ec6f72ae414bff10da639be06e714c07
Kim Vo <kimlongt.vo@gmail.com>
2020-05-26 17:32:15 -0700

    created Add logs actions and reducers for redux

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
     const [message, setMessage] = useState('');
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState('');
@@ -10,8 +13,17 @@ const AddLogModal = () => {
         if (message === '' || tech === '') {
             M.toast({ html: 'Please enter a message and tech' });
         } else {
-            console.log(message, tech, attention);
-            
+            const newLog ={
+                message,
+                attention,
+                tech,
+                date: new Date()
+            }
+
+            addLog(newLog);
+
+            M.toast({ html: `Log added by ${tech}`});
+
             //  Clear fields
             setMessage('');
             setTech('');
@@ -68,6 +80,7 @@ const AddLogModal = () => {
                                     value={attention}
                                     onChange={(e) => setAttention(!attention)}
                                 />
+                                <span>Needs Attention</span>
                             </label>
                         </p>
                     </div>
@@ -86,9 +99,13 @@ const AddLogModal = () => {
     );
 };
 
+AddLogModal.propTypes = {
+    addLog: PropTypes.func.isRequired,
+}
+
 const modalStyle = {
     width: '75%',
     height: '75%',
 };
 
-export default AddLogModal;
+export default connect(null, { addLog }) (AddLogModal);
