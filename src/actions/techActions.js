1a6acfbcdb08ab919134339adaaa9e8554969eaa
Kim Vo <kimlongt.vo@gmail.com>
2020-05-27 14:02:49 -0700

    created add tech actions and reducers for AddTechModal component

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -26,6 +26,31 @@ export const getTechs = () => async (dispatch) => {
     }
 };
 
+//  Add Tech to server
+export const addTech = (tech) => async (dispatch) => {
+    try {
+        setLoading();
+        const res = await fetch('/techs', {
+            method: 'POST',
+            body: JSON.stringify(tech),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_TECH,
+            payload: data,
+        });
+    } catch (err) {
+        dispatch({
+            type: TECH_ERRORS,
+            payload: err.response.statusText,
+        });
+    }
+};
+
 //  Set loading to true
 export const setLoading = () => {
     return {
