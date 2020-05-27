7225b8bd2dd92e9fb827a2f0373f416e5d75b59e
Kim Vo <kimlongt.vo@gmail.com>
2020-05-26 17:13:35 -0700

    connected Redux to Logs component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -14,6 +14,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 // }
 
 //  Refactor function from above
+//  Get logs from server
 export const getLogs = () => async (dispatch) => {
     try {
         setLoading();
